c2dfe9835f9bd2d2597cd9e452d66c28013c9ed0
eugene <eugenejhonravina@gmail.com>
2023-10-05 00:54:31 +0800

    initial commit project setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(
+    <Router basename="/">
+      <App />
+    </Router>,
+  );
+  const linkElement = screen.getByText(/Our Home/i);
   expect(linkElement).toBeInTheDocument();
 });
